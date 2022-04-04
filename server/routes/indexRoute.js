966f575c4be12c93fd4d65fee172bf5ce446f919
andrewpatasik <andrew.yehezkiel17@gmail.com>
2022-04-04 15:14:51 +0800

    create admin login view

--- server/routes/indexRoute.js
+++ server/routes/indexRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
-router.get('/', passport.authenticate('jwt', { session: false, failureRedirect: '/auth/login' }), (req, res) => {
+router.get('/', passport.authenticate('jwt', { session: false, failureRedirect: '/auth/login/' }), (req, res) => {
   res.render('index');
 });
 
