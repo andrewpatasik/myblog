3b3b5d1d8f52e4496037464ddda0aae0fcdc1ea3
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2022-06-04 10:25:05 +0800

    implementing redux

--- client/src/helper/server.js
+++ client/src/helper/server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const server = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://andrewpatasik.herokuapp.com/api",
 });
 
 export default server;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { createStore } from 'redux';
 
 import App from './App';
 
+// const store = createStore();
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
